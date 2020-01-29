c7be7dcd6de3b734d99edcf15b4ec6be28793da1
Sourav De <srvz39@gmail.com>
2020-01-29 05:54:43 +0530

    datetime format fixed

--- nifty_backtest.py
+++ nifty_backtest.py
@@ -11,4 +11,10 @@ index = 'BANKNIFTY.txt'
 df = pd.read_csv(srcFolder + index, index_col=False, names=['ticker', 'date', 'time', 'open', 'high', 'low', 'close', 'volume'])
 # Rename all of the columns, keeping them in order
 # df.columns = ['ticker', 'date', 'time', 'open', 'high', 'low','close','volume']
+df['datetime'] = df[['date', 'time']].astype(str).apply(' '.join, axis=1)
+# df['date'] = pd.to_datetime(df['date'], format="%Y%m%d")
+# df['time'] = pd.to_datetime(df['time'], format="%H:%M").dt.time
+# df['time'] = pd.to_timedelta(df['time'], format="%H:%M")
+df['datetime'] = pd.to_datetime(df['datetime'], format="%Y%m%d %H:%M")
 print(df.head())
+print(df[['date', 'time', 'datetime']].dtypes)
